3749ee90aac9b13bc6699f100bf5db8ee29a9655
jaewan0091 <wodhks7868@gmail.com>
2024-07-11 17:58:27 +0900

    npm react-router-dom 설치 및 확인

--- movie_app_24_07_11/src/Router.js
+++ movie_app_24_07_11/src/Router.js
@@ -1,16 +1,17 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { Detail } from "./pages/detail/Detail";
 import { Home } from "./pages/home/Home";
-import { PageNotFound } from "./pages/PageNotFound";
+import { Detail } from "./pages/detail/Detail";
 import { Search } from "./pages/search/Search";
+import { PageNotFound } from "./pages/PageNotFound";
+import { routes } from "./routes";
 
-const Router= () => {
+const Router = () => {
     return (
     <HashRouter>
         <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/detail/:id" element={<Detail/>} />
-            <Route path="/search" element={<Search/>} />
+            <Route path={routes.home} element={<Home/>} />
+            <Route path={routes.detail} element={<Detail/>} />
+            <Route path={routes.search} element={<Search/>} />
             <Route path="/*" element={<PageNotFound/>} />
         </Routes>
     </HashRouter>
