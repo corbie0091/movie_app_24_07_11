70bc63251b414b4cd0954da08cf40fa82933a82b
jaewan0091 <wodhks7868@naver.com>
2024-08-02 17:30:27 +0900

    반응형 수정2차

--- src/components/ScrollToTopBtn.js
+++ src/components/ScrollToTopBtn.js
@@ -4,21 +4,34 @@ import { TbArrowBarToUp } from "react-icons/tb";
 
 const Button = styled.button`
   position: fixed;
-  bottom: 50px;
-  right: 50px;
-  font-size: 40px;
-  background-color: #555;
+  bottom: 20px;
+  right: 20px;
+  width: 60px;
+  height: 60px;
+  border: 2px solid #fff;
+  background-color: transparent;
   color: #fff;
-  border: none;
-  border-radius: 5px;
+  border-radius: 50%;
   cursor: pointer;
   opacity: ${(props) => (props.visible ? 1 : 0)};
   transition: opacity 0.3s;
   z-index: 99;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   svg {
-    padding: 6px;
-    font-size: 40px;
+    font-size: 24px;
+  }
+
+  @media (max-width: 360px) {
+    width: 50px;
+    height: 50px;
+    border-width: 1px;
+
+    svg {
+      font-size: 20px;
+    }
   }
 `;
 
